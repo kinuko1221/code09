4440ab15e37100f914bd9bae3fe4c91a80ba344b
kinuko1221 <kinuko1221@net44-dhcp151.sfc.keio.ac.jp>
2016-06-23 12:20:58 +0900

    練習問題1~7

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,40 @@
 console.log("start audio.js");
+
+function play(){
+var audio;
+audio = document.querySelector("audio");
+
+audio.play();
+}
+
+function pause(){
+var audio;
+audio = document.querySelector("audio");
+
+audio.pause();
+}
+
+function skip10sec(){
+var audio;
+audio = document.querySelector("audio");
+
+audiocurrentTime = audiocurrentTime + 10
+}
+
+function play(event){
+  var player = document.querySelector("audio");
+  console.log("play");
+  player.play();
+}
+
+var playButton = document.querySelector("[data-role=play]");
+playButton.addEventListener("click", play);
+
+function pause(event){
+  var player = document.querySelector("audio");
+  console.log("play");
+  player.pause();
+}
+
+var pauseButton = document.querySelector("[data-role=pause]");
+pauseButton.addEventListener("click", pause);
